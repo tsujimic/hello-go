3c5822b0ca68ef2f13d2e108f56b6a14c0a3cfcc
takashi.tsuji <takashi.tsuji@icloud.com>
2022-12-20 10:50:24 +0900

    update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,11 +1,11 @@
 package main
 
-// import "testing"
+import "testing"
 
-// func TestGreeting(t *testing.T) {
-// 	want := "Hello"
-// 	got := Greeting()
-// 	if want != got {
-// 		t.Errorf("got %q want %q", got, want)
-// 	}
-// }
+func TestGreeting(t *testing.T) {
+	want := "Hello"
+	got := Greeting()
+	if want != got {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
